860c455530fd05e08e0b0cadf7a6a8ab9e20231d
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-08 23:55:27 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/com/example/kusithm_part_study/KusithmPartStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kusithm_part_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusithmPartStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KusithmPartStudyApplication.class, args);
+    }
+
+}
