f9a8b8e2e9cd39e9c0d0fe2de2ded978fcd0f92d
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-09 15:34:21 +0900

    [chore] global package 설정

---
+++ src/main/java/com/example/kusithm_part_study/global/error/exception/ConflictException.java
@@ -0,0 +1,12 @@
+package com.example.kusithm_part_study.global.error.exception;
+
+public class ConflictException extends BusinessException {
+    public ConflictException() {
+        super(ErrorCode.CONFLICT);
+    }
+
+    public ConflictException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
+
---
+++ src/main/java/com/example/kusithm_part_study/global/error/exception/InvalidValueException.java
@@ -0,0 +1,11 @@
+package com.example.kusithm_part_study.global.error.exception;
+
+public class InvalidValueException extends BusinessException {
+    public InvalidValueException() {
+        super(ErrorCode.BAD_REQUEST);
+    }
+
+    public InvalidValueException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
